91c3b3d581e9c86cc132d788449e6a28d37eb809
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-12-22 14:23:39 -0800

    Modify cookbook template to fit our needs.
    Drops the default recipe in favor of a server recipe which will be one
    of the two roles.
    Fixes up test_kitchen and the cookbook metadata.rb.

--- rmf_demo/metadata.rb
+++ rmf_demo/metadata.rb
@@ -1,19 +1,9 @@
 name 'rmf_demo'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures rmf_demo'
+maintainer 'Steven! Ragnarök'
+maintainer_email 'steven@openrobotics.org'
+license 'Apache 2.0'
+description 'Installs/Configures hosts for an RMF demo'
 version '0.1.0'
-chef_version '>= 15.0'
+chef_version '>= 16.0', '< 17.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/rmf_demo/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/rmf_demo'
+source_url 'https://github.com/osrf/rmf_demo_project'

c3212095cd5e6ddacb14706dcbb78a97f2639564
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-12-22 21:39:04 -0800

    Add a wireguard interface to the server.

--- rmf_demo/recipes/server.rb
+++ rmf_demo/recipes/server.rb
@@ -1,5 +1,31 @@
-#
-# Cookbook:: rmf_demo
-# Recipe:: default
-#
-# Copyright:: 2020, The Authors, All Rights Reserved.
+apt_update
+package 'wireguard'
+
+execute 'networkctl-reload' do
+  command 'networkctl reload'
+  action :nothing
+end
+
+
+wireguard_key = search(:rmf_demo_wireguard_peers, "id:server").first
+wireguard_peers = search(:rmf_demo_wireguard_peers, "NOT id:server")
+template '/etc/systemd/network/wg0.netdev' do
+  owner 'root'
+  group 'systemd-network'
+  mode '0640'
+  variables Hash[
+    private_key: wireguard_key['private_key'],
+    wireguard_peers: wireguard_peers
+  ]
+  notifies :run, 'execute[networkctl-reload]'
+end
+
+template '/etc/systemd/network/10-wg0.network' do
+  owner 'root'
+  group 'systemd-network'
+  mode '0640'
+  variables Hash[
+  ]
+  notifies :run, 'execute[networkctl-reload]'
+end
+

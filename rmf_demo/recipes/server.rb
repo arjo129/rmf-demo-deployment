a94fc5c2075f22290d6dc8430775032251605baf
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-12-29 12:26:15 -0800

    Add Gazebo, ROS, and RMF repositories.

--- rmf_demo/recipes/server.rb
+++ rmf_demo/recipes/server.rb
@@ -1,4 +1,5 @@
 apt_update
+
 package 'wireguard'
 
 execute 'networkctl-reload' do
@@ -29,3 +30,23 @@ template '/etc/systemd/network/10-wg0.network' do
   notifies :run, 'execute[networkctl-reload]'
 end
 
+# Set up ROS 2 and RMF
+apt_repository 'ros2' do
+  uri 'http://packages.ros.org/ros2/ubuntu'
+  components %w[main]
+  key 'https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc'
+end
+
+apt_repository 'gazebo' do
+  uri 'https://packages.osrfoundation.org/gazebo/ubuntu-stable/'
+  components %w[main]
+  key 'https://packages.osrfoundation.org/gazebo.key'
+end
+
+apt_repository 'rmf' do
+  uri 'http://rmf.servehttp.com/ubuntu/main'
+  components %w[main]
+  key 'http://rmf.servehttp.com/repos.key'
+end
+
+package 'ros-foxy-demos'
